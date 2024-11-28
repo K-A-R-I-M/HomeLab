992381f805e9de76314aa9c9380d3da9e42f31ff
K-A-R-I-M <karim.benothman@outlook.com>
2024-11-28 18:52:03 +0100

    [DEV] add terraform config

---
+++ terraform/general.tfvars
@@ -0,0 +1,7 @@
+proxmox_api_url          = ""
+proxmox_api_token_id     = ""
+proxmox_api_token_secret = ""
+proxmox_node = ""
+pm_template = ""
+sshkey = ""
+private_key_path = ""
---
+++ terraform/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc6"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure     = true
+}
+
+resource "proxmox_vm_qemu" "arch-karim-nodes" {
+  agent       = 1
+  count       = 3
+  name        = "arch-karim-nodes-${count.index + 1}"
+  target_node = var.proxmox_node
+  bios        = "ovmf" # UEFI
+  onboot      = true
+  cores       = 2
+  sockets     = 2
+  memory      = 8192
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "ide2"
+  boot        = "order=scsi0;ide2;net0"
+
+  disks {
+    ide {
+      ide2 {
+        cdrom {
+          iso = "local:iso/archlinux-2024.11.28-x86_64.iso"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size = "32G"
+          storage = "local-lvm"
+          format = "raw"
+        }
+      }
+    }
+  }
+  
+  
+  network {
+    id = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+    tag = null
+  }
+
+  serial {
+    id = 0
+    type = "socket"
+  }
+
+  sshkeys   = var.sshkey
+  
+}
